2fe2e8eec4f1146dcaa9fad487b58f8f9590f9f4
Neelesh Patil <104721593+neelesh-patil-4417@users.noreply.github.com>
2024-06-04 15:41:16 +0530

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('identity_reconciliation.urls'))
+    path('api/', include('identity_reconciliation.urls')),
+    path('',include('identity_reconciliation.urls'))
 ]
